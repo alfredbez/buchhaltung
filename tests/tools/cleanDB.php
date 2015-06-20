7fcc69211959240d16c1635fae7a02fcad17756e
Alfred Bez <alfred.bez@googlemail.com>
2015-06-20 15:27:16 +0200

    added mysql test access data

--- tests/tools/cleanDB.php
+++ tests/tools/cleanDB.php
@@ -1,7 +1,14 @@
 <?php
 function cleanDB()
 {
-  require 'php/mysql_config.php';
+
+  if (getenv('mysqlhost') || file_exists(ROOT_DIR.'.test.lock')) {
+      $mysqlhost = getenv('mysqlhost') ?: 'localhost';
+      $mysqluser = getenv('mysqluser') ?: 'dbuser';
+      $mysqlpwd = getenv('mysqlpwd') ?: '123';
+      $mysqldb = getenv('mysqldb') ?: 'buchhaltung_test';
+  }
+
   $cmd = 'mysql -u ' . $mysqluser . ' -p' . $mysqlpwd . ' ' . $mysqldb . ' < db/test.sql';
   // mute command
   $cmd .= ' > /dev/null 2>&1';
