71f7504d01b771180375bcd5867a9676b4456c2f
Alfred Bez <alfred.bez@googlemail.com>
2015-06-23 12:30:27 +0200

    detect travis env

--- tests/tools/cleanDB.php
+++ tests/tools/cleanDB.php
@@ -2,19 +2,23 @@
 function cleanDB()
 {
 
-  if (getenv('mysqlhost') || file_exists(ROOT_DIR.'.test.lock')) {
-      $mysqlhost = getenv('mysqlhost') ?: '127.0.0.1';
-      $mysqluser = getenv('mysqluser') ?: 'root';
-      $mysqlpwd = getenv('mysqlpwd') ?: '';
-      $mysqldb = getenv('mysqldb') ?: 'buchhaltung_test';
+  if(getenv('TRAVIS'))
+  {
+    $cmd = 'mysql -u root --password="" buchhaltung_test < db/test.sql';
+    print_r($cmd);
+    print_r(shell_exec($cmd));
+    print_r("\n");
+    die('TRAVIS and die');
+  }
+  else
+  {
+    $mysqlhost = getenv('mysqlhost') ?: '127.0.0.1';
+    $mysqluser = getenv('mysqluser') ?: 'root';
+    $mysqlpwd = getenv('mysqlpwd') ?: '123';
+    $mysqldb = getenv('mysqldb') ?: 'buchhaltung_test';
   }
 
-  if($mysqlpwd === '')
-  {
-    $passwordFlag = '--password=""';
-  }
-  $passwordFlag = $passwordFlag ?: '-p' . $mysqlpwd;
-  $cmd = 'mysql -u ' . $mysqluser . ' ' . $passwordFlag . ' ' . $mysqldb . ' < db/test.sql';
+  $cmd = 'mysql -u ' . $mysqluser . ' -p' . $mysqlpwd . ' ' . $mysqldb . ' < db/test.sql';
   // mute command
   // $cmd .= ' > /dev/null 2>&1';
   die(var_dump(shell_exec($cmd)));
