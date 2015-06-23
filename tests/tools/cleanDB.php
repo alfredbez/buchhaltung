48369d56bce5bf8e083dc8c044409a12dfe107c8
Alfred Bez <alfred.bez@googlemail.com>
2015-06-23 12:33:27 +0200

    remove debug output

--- tests/tools/cleanDB.php
+++ tests/tools/cleanDB.php
@@ -5,10 +5,6 @@ function cleanDB()
   if(getenv('TRAVIS'))
   {
     $cmd = 'mysql -u root --password="" buchhaltung_test < db/test.sql';
-    print_r($cmd);
-    print_r(shell_exec($cmd));
-    print_r("\n");
-    die('TRAVIS and die');
   }
   else
   {
@@ -16,10 +12,10 @@ function cleanDB()
     $mysqluser = getenv('mysqluser') ?: 'root';
     $mysqlpwd = getenv('mysqlpwd') ?: '123';
     $mysqldb = getenv('mysqldb') ?: 'buchhaltung_test';
+    $cmd = 'mysql -u ' . $mysqluser . ' -p' . $mysqlpwd . ' ' . $mysqldb . ' < db/test.sql';
   }
 
-  $cmd = 'mysql -u ' . $mysqluser . ' -p' . $mysqlpwd . ' ' . $mysqldb . ' < db/test.sql';
   // mute command
-  // $cmd .= ' > /dev/null 2>&1';
-  die(var_dump(shell_exec($cmd)));
+  $cmd .= ' > /dev/null 2>&1';
+  shell_exec($cmd);
 }
