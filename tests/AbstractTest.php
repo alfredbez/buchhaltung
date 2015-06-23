15329f5c42c62460a0f2f101cd28abb704580bc7
Alfred Bez <alfred.bez@googlemail.com>
2015-06-23 14:13:16 +0200

    overwrite execute method
    see also https://github.com/laracasts/Integrated/pull/99

--- tests/AbstractTest.php
+++ tests/AbstractTest.php
@@ -6,4 +6,20 @@ require_once 'Traits/TestTrait.php';
 
 abstract class AbstractTest extends IntegrationTest {
   use TestTrait;
+
+    /**
+     * Execute the query.
+     *
+     * @param  \PDOStatement $query
+     * @return \PDOStatement
+     */
+    protected function execute($query)
+    {
+      $query->execute($this->bindings);
+
+      $this->bindings = [];
+      $this->wheres   = [];
+
+      return $query;
+    }
 }
