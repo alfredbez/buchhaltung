dd4ba4c3664a59685ebe9c2087d52749ef6a3823
Alfred Bez <alfred.bez@googlemail.com>
2015-06-23 13:10:24 +0200

    start php webserver

--- integrated.php
+++ integrated.php
@@ -1,7 +1,13 @@
 <?php
 
+$baseUrl = 'http://127.0.0.1/igor';
+if(getenv('TRAVIS'))
+{
+  $baseUrl = 'http://localhost:8000';
+}
+
 return [
-  'baseUrl' => 'http://127.0.0.1/igor',
+  'baseUrl' => $baseUrl,
   'pdo' => [
     'connection' => 'mysql:host='.getenv('mysqlhost').';dbname='.getenv('mysqldb'),
     'username' => getenv('mysqluser'),
