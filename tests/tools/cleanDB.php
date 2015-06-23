c464e754508f7c0cfff16e701b52e845f39010d5
Alfred Bez <alfred.bez@googlemail.com>
2015-06-23 09:48:45 +0200

    show mysql import output

--- tests/tools/cleanDB.php
+++ tests/tools/cleanDB.php
@@ -11,6 +11,6 @@ function cleanDB()
 
   $cmd = 'mysql -u ' . $mysqluser . ' -p' . $mysqlpwd . ' ' . $mysqldb . ' < db/test.sql';
   // mute command
-  $cmd .= ' > /dev/null 2>&1';
+  // $cmd .= ' > /dev/null 2>&1';
   shell_exec($cmd);
 }
